7719cb90138c7a7e383cb26e0aea82ea13dbe8dd
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-11-04 13:03:31 +0700

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Screenshot API using express & pupetter
+
+Yes, screenshot a website using API
+
+example:
+
+<pre><code>
+http://localhost:3000/shot?url=<b>https://wisesa.dev</b>&width=<b>600</b>&height=<b>600</b>&encode=<b>true</b>
+</code></pre>
+
+parameters
+
+- **url**: your url (required)
+- **width**: image width
+- **height**: image height
+- **encode**: return image in base64 (default:false)
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const app = express();
+const port = 3000;
+
+const getScreenshot = async (url, config) => {
+  let options = {
+    fullPage: true,
+    width: 800,
+    height: 600,
+    ...config,
+  };
+  try {
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      defaultViewport: {
+        width: options.width,
+        height: options.height,
+      },
+    });
+    const page = await browser.newPage();
+    // await page.setViewport({ width: options.width, height: options.height });
+    await page.goto(url);
+    const image = await page.screenshot({ fullPage: options.fullPage });
+    await browser.close();
+    return image;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hewwo World!");
+});
+
+app.get("/shot", async (req, res) => {
+  const { url, encode, width, height } = req.query;
+  if (!url) {
+    res.status(400).send("url query not defined, BAD REQUEST!");
+  }
+  const result = await getScreenshot(url, {
+    width: parseInt(width),
+    height: parseInt(height),
+  });
+  if (encode) {
+    res.json({
+      image: `data:image/jpeg;base64,${Buffer(result).toString("base64")}`,
+    });
+  } else {
+    res.set("Content-Type", "image/png");
+    res.send(result);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
