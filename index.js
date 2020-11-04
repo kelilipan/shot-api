5f45e971bee23ae77bc7c9e33bbedb6cd62a2837
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-11-04 13:11:58 +0700

    fix process port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const getScreenshot = async (url, config) => {
   let options = {
